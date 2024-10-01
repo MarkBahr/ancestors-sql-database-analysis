2b6c58a47dc7262f149056efef38a5550221e474
MarkBahr <bahrmark147@gmail.com>
2024-10-01 16:26:02 -0500

    Wrote create table and insert statements

--- data-wrangling/data-structure/fam_hist_insert_data.sql
+++ data-wrangling/data-structure/fam_hist_insert_data.sql
@@ -1,9 +1,42 @@
-
-
+/*
+    Name: Mark Bahr
+    Project: Family History SQL Database & Analysis
+    Tasks: Use PostgreSQL to 
+        - Insert the correct data into each table using the cleaned data in the ancestors and soureces_original tables.
+*/
 
 -- Use the following syntax
-INSERT INTO table_2 (col1, col2, col3, ...)
-SELECT col1, col2, col3, ...
-FROM table_1
-WHERE cond;
+-- INSERT INTO table_2 (col1, col2, col3, ...)
+-- SELECT col1, col2, col3, ...
+-- FROM table_1
+-- WHERE cond;
 
+
+-- Insert data into SOURCES table
+INSERT INTO sources (source_id, source_creator, source_title, source_type, source_format, source_year, source_month, source_day, source_location, source_description)
+SELECT source_id, source_creator, source_title, source_type, source_format, source_year, source_month, source_day, source_location, source_description
+from sources_original;
+
+
+-- Insert data into PERSONS table
+INSERT INTO persons (person_id, first_name, last_name, gender, living)
+SELECT person_id, first_name, last_name, gender, living
+FROM ancestors;
+
+
+-- Insert data into BIRTHS table
+INSERT INTO births (birth_id, person_id, birth_year, birth_month, birth_day, birth_city, birth_state, birth_country, mother_id, father_id, source_id)
+SELECT birth_id, person_id, birth_year, birth_month, birth_day, birth_city, birth_state, birth_country, mother_id, father_id, birth_source_id
+FROM ancestors;
+
+
+-- Insert data into MARRIAGES table
+INSERT INTO marriages (marraige_id, person_id, marraige_year, marraige_month, marraige_day, marraige_city, marraige_state, marraige_country, source_id)
+SELECT marraige_id, person_id, marraige_year, marraige_month, marraige_day, marraige_city, marraige_state, marraige_country, marriage_source_id
+from ancestors;
+
+
+-- Insert data into DEATHS table
+INSERT INTO deaths (death_id, person_id, death_year, death_month, death_day, death_city, death_state, death_country, source_id)
+SELECT death_id, person_id, death_year, death_month, death_day, death_city, death_state, death_country, death_source_id
+from ancestors;
--- data-wrangling/data-structure/fami_hist_ddl.sql
+++ data-wrangling/data-structure/fami_hist_ddl.sql
@@ -1,60 +1,69 @@
 /*
     Name: Mark Bahr
-    Project: Family History Database
-    Skill: Use PostgreSQL to 
-        - Import data using simple CREATE TABLE and COPY statements
-        - Add new columns and use UPDATE to set values, then drop a column
-        - Filter, group and sort data using WHERE, GROUP BY and ORDER BY
+    Project: Family History SQL Database & Analysis
+    Tasks: Use PostgreSQL to 
+        - CREATE tables and columns with specified data types
+        - CREATE constraints, such as Primary keys, check constraints, foreign keys
 */
 
--- Find out if it's advisable to separate out year, month, day, or to list a date. 
--- Find out if postgres convention is to name pks and fks with pk or fk first? 
--- Find out max length of a few columns
--- Add check constraints and primary/foreign keys
--- Find sources for each event
 
+-- TO DO BEFORE executing CREATe TABLE statements: 
+    -- Create data for the fictitious (but believable) source info for each event for each person. Add to the ancestor's table the brith_source_id, marriage_source_id, and death_source_id.
 
-CREATE TABLE Persons (
-    person_id INT,
-    first_name VARCHAR(40),
-	last_name VARCHAR(40),
-    gender char(1),
-    living boolean
-);
+-- Note: constraint naming pattern: table_column_constraint. Example: births_birth_id_pk
 
+-- Define table for sources
 CREATE TABLE sources (
-    source_id INT,
-    source_creator VARCHAR,
-    source_title VARCHAR,
-    source_type VARCHAR,
-    source_format VARCHAR,
+    source_id CHAR(6),
+    source_creator VARCHAR(100),
+    source_title VARCHAR(100),
+    source_type VARCHAR(20),
+    source_format VARCHAR(20),
     source_year INT,
     source_month VARCHAR(9),
     source_day INT,
     source_location VARCHAR(150),
-    source_description TEXT
+    source_description TEXT,
+    CONSTRAINT sources_source_id_pk PRIMARY KEY (source_id),
 );
 
+-- Define table for each person's name, gender, whether living
+CREATE TABLE Persons (
+    person_id CHAR(5),
+    first_name VARCHAR(50),
+	last_name VARCHAR(50),
+    gender char(1),
+    living VARCHAR(8)
+);
+
+-- Define table for births
 CREATE TABLE births (
-	birth_id INT,
-    person_id INT,
-	birth_year INT CHECK(birth_year > 1500 AND birth_year < 2000),
+	birth_id CHAR(5),
+    person_id CHAR(6),
+	birth_year INT CHECK(birth_year > 1600 AND birth_year < 2000),
     birth_month INT CHECK (birth_month >= 1 AND birth_month < 13),
     birth_day INT CHECK (birth_day >= 1 AND birth_day < 32),  
 	birth_city VARCHAR(100),
     birth_state VARCHAR(80),
     birth_country VARCHAR(70),
+    mother_id CHAR(5),
+    father_id CHAR(5),
     source_id INT,
-	CONSTRAINT births_pk PRIMARY KEY (birth_id),
-    CONSTRAINT birth_person_fk FOREIGN KEY (person_id)
+	CONSTRAINT births_birth_id_pk PRIMARY KEY (birth_id),
+    CONSTRAINT births_person_id_fk FOREIGN KEY (person_id)
     REFERENCES persons,
-    CONSTRAINT birth_source_fk Foreign Key (source_id)
+    CONSTRAINT births_mother_id_fk FOREIGN KEY (person_id)
+    REFERENCES persons,
+    CONSTRAINT births_father_id_fk FOREIGN KEY (person_id)
+    REFERENCES persons,
+    CONSTRAINT births_source_id_fk Foreign Key (source_id)
 	REFERENCES sources (source_id)
 );
 
+-- Define table for deaths
 CREATE TABLE deaths (
-	death_id INT,
-    person_id INT,
+	death_id CHAR(5),
+    person_id CHAR(5),
 	death_year INT,
     daath_month VARCHAR(9),
     death_day INT,
@@ -62,17 +71,18 @@ CREATE TABLE deaths (
     death_state VARCHAR(80),
     death_country VARCHAR(70),
     source_id INT,
-    CONSTRAINT death_pk PRIMARY KEY (death_id),
-	CONSTRAINT death_person Foreign Key (person_id)
+    CONSTRAINT deaths_death_id_pk PRIMARY KEY (death_id),
+	CONSTRAINT deaths_person_id_fk Foreign Key (person_id)
 	REFERENCES persons (person_id),
-    CONSTRAINT death_source_fk Foreign Key (source_id)
+    CONSTRAINT deaths_source_id_fk Foreign Key (source_id)
 	REFERENCES sources (source_id)
 );
 
+-- Define table for marriages
 CREATE TABLE marriages (
-	marriage_id INT,
-	Groom_Id INT NOT NULL,
-	Bride_Id INT NOT NULL,
+	marriage_id CHAR(4),
+	Groom_Id CHAR(5) NOT NULL,
+	Bride_Id CHAR(5) NOT NULL,
     marriage_year INT,
     marriage_month VARCHAR(9),
     marriage_day INT,
@@ -80,26 +90,25 @@ CREATE TABLE marriages (
     marriage_state VARCHAR(80),
     marriage_country VARCHAR(70),
     source_id INT,
-	CONSTRAINT marriage_pk Primary Key (marriage_Id),
-	CONSTRAINT FK_Groom_Id Foreign Key (groom_Id)
+	CONSTRAINT marriages_marriage_id_pk Primary Key (marriage_Id),
+	CONSTRAINT marriages_groom_id_fk Foreign Key (groom_Id)
 	REFERENCES persons (person_id),
-	CONSTRAINT FK_Bride_Id Foreign Key (bride_Id)
+	CONSTRAINT marriages_bride_id_fk Foreign Key (bride_Id)
 	REFERENCES persons (person_id),
-    CONSTRAINT marriage_source_fk Foreign Key (source_id)
+    CONSTRAINT marriages_source_id_fk Foreign Key (source_id)
 	REFERENCES sources (source_id)
 );
 
-CREATE TABLE relationships (
-    relationship_id,
-    person_id int,
-    relationship_type VARCHAR(12),
-    relation_id int,
-    source_id int,
-    CONSTRAINT relate_person_fk Foreign Key (person_id)
-	REFERENCES persons (person_id),
-    CONSTRAINT relative_fk Foreign Key (relation_id)
-	REFERENCES persons (person_id)
-    CONSTRAINT relate_source_fk Foreign Key (source_id)
-	REFERENCES sources (source_id)
-);
-
+-- Optional relationships table
+-- CREATE TABLE relationships (
+--     relationship_id CHAR(7),
+--     person_id CHAR(5),
+--     relationship_type VARCHAR(12),
+--     source_id int,
+--     CONSTRAINT relationships_person_fk Foreign Key (person_id)
+-- 	REFERENCES persons (person_id),
+--     CONSTRAINT relative_fk Foreign Key (relation_id)
+-- 	REFERENCES persons (person_id)
+--     CONSTRAINT relate_source_fk Foreign Key (source_id)
+-- 	REFERENCES sources (source_id)
+-- );
