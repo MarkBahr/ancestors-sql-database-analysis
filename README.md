106ef289b7b0237fdfc46808c96d7b754eaafa27
MarkBahr <bahrmark147@gmail.com>
2024-10-08 09:46:16 -0500

    New Select queries added

--- README.md
+++ README.md
@@ -1,18 +1,20 @@
 # SQL Data Analysis for Genealogy
 
-SQL data analysis for family history data with tasks related to data wrangling (cleaning, and structuring), data analysis and visualizations using Tableau. The data is from a ficticious genealogist who desires to have additional data storage and data analysis for his clients.
+This project uses the SQL database language to load, clean, organize, analyze and visualize family history data. The SQL tasks included data wrangling (cleaning, and structuring), and data analysis and the data visualization will be done using Tableau. The presentation will use Miscrosoft PowerPoint and Tableau. The data is from a ficticious genealogist who desires to have additional data storage and data analysis for his clients.
 
 The project folders follow the following format:
 
 #### Data Wrangling
 
-- Data cleaning, which includes the raw data and SQL files to create a database, import the data, clean the data, and splitting date and location data into multiple columns
-- Data Structure, which includes ER diagram and the SQL files to create the storage tables and insert the cleaned data.
+- Data cleaning, which includes the files for the raw data and SQL files for cleaning. Using SQL I created a database, imported the data into a single table, cleaned the data, and split the location and date columns into multiple columns.
+- The Data Structure folder includes a csv file with cleaned data, an ER diagram and the SQL files to create the storage tables and insert the cleaned data. This is where the data is organized into separate tables with required relationships and check constraints.
 
 #### Data Analysis
 
-- Analyze the data by finding summary statistics, and trends.
-- Gain the answers to important or interesting questions. For example, which country is home for the greatest number of your ancestors? Even getting simple answers from data that is organized into separate tables can require complex joins and subqueries. For example, one of the most complex queries I wrote so far is the query that returns a list of the names of a each ancestor's along with the names of their parents and grandparents.
+- Analyze the data by finding summary statistics
+- Gain the answers to important or interesting questions.
+
+For example, which country is home for the greatest number of your ancestors? Or, which ancestors had the longest life span? These select queries are much more complicated than the select queries seen in the data wrangling section. Getting simple answers from data that is organized into separate tables can require complex joins and subqueries. For example, one of the more difficult queries I wrote so far in this project is the query that returns a list of the names of a each ancestor along with the names of their parents and grandparents. The query that found ancestors who had emigrated was also difficult, because emigration might not be proved just because a person's birth and death country were different. To provide further likelihood of emigration, I also checked to see if their children were born or married in the parent's death_country.
 
 #### Data Visualization
 
--- data-analyis/analysis-dml.sql
+++ data-analyis/analysis-dml.sql
@@ -26,39 +26,7 @@ ORDER BY ancestor_count DESC;
 
 
 --------------------------------------------------------------------
-/*	         QUERY 2: ANCESTORS WITH LONGEST LIFE SPANS           */
---------------------------------------------------------------------
--- List the 10 ancestors who lived the longest in descending order by their life span. Include their birth and death countries.
-SELECT  P.first_name, P.last_name,
-        -- Calculate life span by finding the difference between the death date and birth date. Here, I cast the integer values for month, day and year to varchar so that the to_date function works correctly.
-        FLOOR((TO_DATE(D.death_month::VARCHAR || '/' || D.death_day::VARCHAR || '/' || D.death_year::VARCHAR , 'MM/DD/YYYY')
-        - TO_DATE(B.birth_month::VARCHAR || '/' || B.birth_day::VARCHAR || '/' || B.birth_year::VARCHAR , 'MM/DD/YYYY')) / 365.25) life_span, 
-        B.birth_country, D.death_country
-from births B 
-    INNER JOIN deaths D ON B.person_id = D.person_id
-    INNER JOIN persons P ON P.person_id = B.person_id
-ORDER BY life_span DESC
-LIMIT 10;
-
-
-
---------------------------------------------------------------------
-/*	     QUERY 3: BRIDES & GROOMS MARRIED IN ENGLAND          */
---------------------------------------------------------------------
--- List marriages held in England with names of grooms and brides, sort by groom's last name in alphabetical order.
-SELECT  groom.first_name || ' ' || groom.last_name groom_name,
-        bride.first_name || ' ' || bride.last_name bride_name,
-        M.marriage_year, M.marriage_city
-from    marriages M 
-    INNER JOIN persons groom ON M.groom_id = groom.person_id
-    INNER JOIN persons bride ON M.bride_id = bride.person_id
-WHERE marriage_country = 'England'
-ORDER BY groom.last_name;
-
-
-
---------------------------------------------------------------------
-/*	            QUERY 4: PARENTS AND GRANDPARENTS                */
+/*	            QUERY 2: PARENTS AND GRANDPARENTS                */
 --------------------------------------------------------------------
 -- For each person with grandparents listed, include the person's name along with their parents and grandparents. Sort the child names by birth year.
 WITH parents AS (
@@ -78,3 +46,86 @@ FROM parents P
         INNER JOIN parents paternal ON P.father_id = paternal.person_id
         INNER JOIN parents maternal ON P.mother_id = maternal.person_id
 ORDER BY P.birth_year DESC;
+
+
+
+--------------------------------------------------------------------
+/*	         QUERY 3: ANCESTORS WITH LONGEST LIFE SPANS           */
+--------------------------------------------------------------------
+-- List the 10 ancestors who lived the longest in descending order by their life span. Include their birth and death countries.
+SELECT  P.first_name, P.last_name,
+        -- Calculate life span by finding the difference between the death date and birth date. Here, I cast the integer values for month, day and year to varchar so that the to_date function works correctly.
+        FLOOR((TO_DATE(D.death_month::VARCHAR || '/' || D.death_day::VARCHAR || '/' || D.death_year::VARCHAR , 'MM/DD/YYYY')
+        - TO_DATE(B.birth_month::VARCHAR || '/' || B.birth_day::VARCHAR || '/' || B.birth_year::VARCHAR , 'MM/DD/YYYY')) / 365.25) life_span, 
+        B.birth_country, D.death_country
+from births B 
+    INNER JOIN deaths D ON B.person_id = D.person_id
+    INNER JOIN persons P ON P.person_id = B.person_id
+ORDER BY life_span DESC
+LIMIT 10;
+
+
+
+--------------------------------------------------------------------
+/*	     QUERY 4: BRIDES & GROOMS MARRIED IN ENGLAND          */
+--------------------------------------------------------------------
+-- List marriages held in England with names of grooms and brides, sort by groom's last name in alphabetical order.
+SELECT  groom.first_name || ' ' || groom.last_name groom_name,
+        bride.first_name || ' ' || bride.last_name bride_name,
+        M.marriage_year, M.marriage_city
+from    marriages M 
+    INNER JOIN persons groom ON M.groom_id = groom.person_id
+    INNER JOIN persons bride ON M.bride_id = bride.person_id
+WHERE marriage_country = 'England'
+ORDER BY groom.last_name;
+
+
+
+--------------------------------------------------------------------
+/*	        QUERY 5: BORN AND DIED IN THE SAME CITY               */
+--------------------------------------------------------------------
+-- Who died in the same city where they were born? List their full name, city, country, and death year.
+SELECT  P.first_name || ' ' || P.last_name full_name, D.death_city, D.death_country, death_year
+FROM    deaths D 
+    INNER JOIN births B ON D.person_id = B.person_id
+    INNER JOIN persons P ON D.person_id = P.person_id
+WHERE B.birth_city = D.death_city
+ORDER BY death_city;
+
+
+
+--------------------------------------------------------------------
+/*	            QUERY 6: EVIDENCE OF EMIGRATION                   */
+--------------------------------------------------------------------
+-- How many ancestors were born in one country, and died in another? In addition, did their children have marriages or births performed in their death country? 
+-- Emigration might not be proved Just because an anscestor died in a different country.
+-- To provide further likelihood of emigration, we'll also look to see if their children were born or married in
+-- the same place where that immigrant died. 
+
+WITH marriage_birth AS (
+    -- First, get a list of every recorded parent and their child's birth and marriage countries
+    WITH child_marriage AS (
+        SELECT groom_id child_id, marriage_country
+        FROM marriages
+        UNION
+        SELECT bride_id child_id, marriage_country
+        FROM marriages
+    )
+    SELECT mother_id parent_id, person_id child_id, birth_country, marriage_country
+    from child_marriage CM INNER JOIN births BM ON CM.child_id = BM.person_id
+    WHERE mother_id IS NOT NULL
+    UNION
+    SELECT father_id parent_id, person_id child_id, birth_country, marriage_country
+    FROM child_marriage CM INNER JOIN births BM ON CM.child_id = BM.person_id
+    WHERE father_id IS NOT NULL
+)
+-- List the desired columns, joins and conditions for those parents
+SELECT  P.first_name || ' ' || P.last_name full_name, D.death_country, 
+        CB.birth_country child_birth_country, CB.marriage_country child_marr_country
+FROM    marriage_birth CB
+    INNER JOIN deaths D ON CB.parent_id = D.person_id
+    INNER JOIN births B ON CB.parent_id = B.person_id
+    INNER JOIN persons P ON CB.parent_id = P.person_id
+WHERE   B.birth_country != D.death_country
+    AND (D.death_country = CB.birth_country OR d.death_country = CB.marriage_country)
+ORDER BY death_city;
